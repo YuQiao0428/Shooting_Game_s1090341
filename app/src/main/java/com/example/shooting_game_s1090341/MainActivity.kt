f9487e60213d28379083996d3dbf1bf7f5f63ba2
YuQiao Huang <leah.huang0428@gmail.com>
2022-05-19 15:16:18 +0800

    第一次Commit

---
+++ app/src/main/java/com/example/shooting_game_s1090341/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.example.shooting_game_s1090341
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
