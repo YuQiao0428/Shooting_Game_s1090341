185a305015e48fcd3c6154a9efbfdf039f2ae935
YuQiao Huang <leah.huang0428@gmail.com>
2022-05-19 16:46:15 +0800

    第二次Commit

--- app/src/main/java/com/example/shooting_game_s1090341/Shooting.kt
+++ app/src/main/java/com/example/shooting_game_s1090341/Shooting.kt
@@ -6,7 +6,7 @@ import android.util.AttributeSet
 import android.view.SurfaceHolder
 import android.view.SurfaceView
 
-class MySurfaceView(context: Context?, attrs: AttributeSet?) : SurfaceView(context, attrs),
+class Shooting(context: Context?, attrs: AttributeSet?) : SurfaceView(context, attrs),
     SurfaceHolder.Callback{
 
     var surfaceHolder: SurfaceHolder
@@ -36,21 +36,16 @@ class MySurfaceView(context: Context?, attrs: AttributeSet?) : SurfaceView(conte
 
     fun drawSomething(canvas:Canvas) {
         var SrcRect: Rect = Rect(0, 0, BG.width, BG.height) //裁切
-        var w:Int = width
-        var h:Int = height
+        var w: Int = width
+        var h: Int = height
         var DestRect: Rect = Rect(0, 0, w, h)
-        canvas.drawBitmap (BG, SrcRect, DestRect, null)
-        val paint = Paint(Paint.ANTI_ALIAS_FLAG)
-        paint.color = Color.BLUE
-        paint.textSize = 50f
-        canvas.drawText("射擊遊戲(作者:黃郁喬)",50f,50f,paint)
+        canvas.drawBitmap(BG, SrcRect, DestRect, null)
 
-
-        //      BGmoveX --
-        // var BGnewX:Int = w + BGmoveX
+        BGmoveX --
+        var BGnewX:Int = w + BGmoveX
 
         // 如果已捲動整張圖，則重新開始
-        /*if (BGnewX <= 0) {
+        if (BGnewX <= 0) {
             BGmoveX = 0
             // only need one draw
             canvas.drawBitmap(BG, BGmoveX.toFloat(), 0f, null)
@@ -60,9 +55,17 @@ class MySurfaceView(context: Context?, attrs: AttributeSet?) : SurfaceView(conte
             canvas.drawBitmap(BG, SrcRect, DestRect, null)
             DestRect = Rect(BGnewX, 0, BGnewX+w, h)
             canvas.drawBitmap(BG, SrcRect, DestRect, null)
-        }*/
+        }
 
 
+        val paint = Paint(Paint.ANTI_ALIAS_FLAG)
+        paint.color = Color.BLUE
+        paint.textSize = 50f
+        canvas.drawText("射擊遊戲(作者:黃郁喬)", 50f, 50f, paint)
     }
+
+
+
+
 
 }
